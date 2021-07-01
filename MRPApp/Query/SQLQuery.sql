ad328070ff371b10fa32dd4ecec2f02819e64538
JO HUI JI <whgmlwl222@naver.com>
2021-07-01 14:00:09 +0900

    20210701 리포트 뷰 IsValidInputs 추가

--- MRPApp/Query/SQLQuery.sql
+++ MRPApp/Query/SQLQuery.sql
@@ -39,3 +39,5 @@ SELECT sch.SchIdx, sch.PlantCode, sch.SchAmount, prc.PrcDate,
 	  GROUP BY smr.SchIdx, smr.PrcDate
  )  AS prc
     ON sch.SchIdx = prc.SchIdx
+ WHERE sch.PlantCode = 'PC010002'
+   AND prc.PrcDate BETWEEN '2021-06-30' AND '2021-07-01'
